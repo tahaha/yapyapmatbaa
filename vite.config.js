e7111163124f63a2a32d3477e7d4c1a2150278aa
tahaha <114696227+tahaha@users.noreply.github.com>
2026-08-26 17:24:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        admin: fileURLToPath(new URL('./admin/index.html', import.meta.url)),
+      },
+    },
+  },
 });
